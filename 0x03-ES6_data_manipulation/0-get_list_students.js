7a381ae3f48e191ca39d62b1b67f05188a26bfe1
nadertoti <nadertoti4@gmail.com>
2024-06-05 01:09:19 +0400

    done

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Return alist of students
+ * @author nader eltayeb <https://gethub.com/nadertoti>
+ */
+export default getListStudents() {
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+}
